30bb335d124c58600c6e20b76ec1baf889055993
Jadon Fowler <jadonflower@gmail.com>
2018-07-01 12:01:09 -0700

    Simplify GBAColor.fromGBA()
    There were a lot of checks for padding the string.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/main/kotlin/io/jadon/sbhs/GBAColor.kt
+++ src/main/kotlin/io/jadon/sbhs/GBAColor.kt
@@ -4,8 +4,8 @@ import java.awt.Color
 
 object GBAColor {
 
-    val fromCache = mutableMapOf<String, Color>()
-    val toCache = mutableMapOf<Color, String>()
+    private val fromCache = mutableMapOf<String, Color>()
+    private val toCache = mutableMapOf<Color, String>()
 
     fun fromGBA(hex: String): Color {
         assert(hex.length == 4) { "Woah! This string isn't 4 characters wide!" }
@@ -34,21 +34,10 @@ object GBAColor {
         val db = Math.min(31, Math.ceil(color.blue / 8.0).toInt())
         val i = Math.min(0x7FFF, (db * 4 * 256) + (dg * 2 * 16) + dr)
 
-        val hex = (if (i < Math.pow(16.0, 3.0)) "0" else "") +
-                (if (i < 256) "0" else "") +
-                (if (i < 16) "0" else "") +
-                Integer.toHexString(i)
+        val hex = Integer.toHexString(i).padStart(4, '0')
 
         toCache[color] = hex
         return hex
     }
 
-    fun toInts(s: String): IntArray {
-        assert(s.length == 4) { "Woah! This string isn't 4 characters wide!" }
-        val c = s.toCharArray()
-        val i = IntArray(2)
-        i[0] = Integer.parseInt(c[0] + "" + c[1], 16)
-        i[1] = Integer.parseInt(c[2] + "" + c[3], 16)
-        return i
-    }
 }
